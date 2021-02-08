8ff0a80de335805976bc875d73cb00e2d7eda9f3
EC2 Default User <ec2-user@ip-172-31-29-213.ec2.internal>
2021-02-08 00:41:51 +0000

    [Add]5-10 投稿詳細ページと一覧ページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
     devise_for :users
     root to: 'homes#top'
     get "homes/about" => "homes#about"
+    resources :books, only: [:create, :new, :index, :show, :destroy]
 end
