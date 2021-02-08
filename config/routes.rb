f6e72b06715befd7686438625f244a30a6db5fc4
EC2 Default User <ec2-user@ip-172-31-29-213.ec2.internal>
2021-02-08 02:52:48 +0000

    [Add]5-10 User詳細ページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
     devise_for :users
     root to: 'homes#top'
     get "homes/about" => "homes#about"
     resources :books, only: [:create, :new, :index, :show, :destroy]
+    resources :users, only: [:show]
 end
