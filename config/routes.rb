ac168226d960b41bc10e42aafe8d79421e50c99a
EC2 Default User <ec2-user@ip-172-31-29-213.ec2.internal>
2021-03-01 12:30:09 +0000

    コメント削除機能とindexページのいいね機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
-  get 'book_comments/create'
-  get 'book_comments/destroy'
     devise_for :users
     get 'users/show'
     root to: 'homes#top'
     get "home/about" => "homes#about"
+    resources :users, only: [:show, :edit, :update, :index]
     resources :books, only: [:create, :new, :index, :show, :destroy, :edit, :update] do
-        resource :book_comments, only: [:create, :destroy]
+        resources :book_comments, only: [:create, :destroy]
         resource :favorites, only: [:create, :destroy]
     end
-    resources :users, only: [:show, :edit, :update, :index]
     post "/users/:id" => "books#create"
     post "/books" => "books#create"
 end
