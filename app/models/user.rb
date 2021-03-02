8e4741361094a8c029a96c671057081b25bbb39b
EC2 Default User <ec2-user@ip-172-31-29-213.ec2.internal>
2021-03-02 07:20:39 +0000

    relationshipモデル記載

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,11 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :follower, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  has_many :following_user, through: :follower, source: :followed
+  has_many :following_user, through: :followed, source: :follower
   attachment :profile_image
   
   validates :name,
@@ -17,4 +21,19 @@ class User < ApplicationRecord
   validates :introduction,
     length: {maximum: 50}
 
+# ユーザーをフォロー
+ def follow(user_id)
+   follow.create(followed_id: user_id)
+ end
+
+#ユーザーのフォローを外す 
+ def funfollow(user_id)
+   follower.find_by(followed_id: user_id).destroy
+ end 
+ 
+# フォロー確認を行う
+ def follow?(user)
+   following_user.include?(user)
+ end 
+ 
 end
