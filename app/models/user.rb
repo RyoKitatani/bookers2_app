f2cf10e144c241f1fda28ef9717623cb7c106e09
EC2 Default User <ec2-user@ip-172-31-29-213.ec2.internal>
2021-03-02 12:01:18 +0000

    showページのフォロー機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,32 +7,34 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :follower, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
 
-  has_many :following_user, through: :follower, source: :followed
-  has_many :following_user, through: :followed, source: :follower
-  attachment :profile_image
-  
   validates :name,
     uniqueness: true,
     length: {minimum: 2, maximum: 20}
 
   validates :introduction,
     length: {maximum: 50}
-
+    
+  # フォロー機能    
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following_user, through: :follower, source: :followed
+  has_many :follower_user, through: :followed, source: :follower
+  attachment :profile_image
+  
 # ユーザーをフォロー
  def follow(user_id)
-   follow.create(followed_id: user_id)
+   follower.create(followed_id: user_id)
  end
 
 #ユーザーのフォローを外す 
- def funfollow(user_id)
+ def unfollow(user_id)
+  # byebug
    follower.find_by(followed_id: user_id).destroy
  end 
  
 # フォロー確認を行う
- def follow?(user)
+ def following?(user)
    following_user.include?(user)
  end 
  
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
     root to: 'homes#top'
     get "home/about" => "homes#about"
     resources :users, only: [:show, :edit, :update, :index]
+    post 'follow/:id' => 'relationships#create', as: 'follow' 
+    post 'unfollow/:id' => 'relationships#destroy', as: 'unfollow' 
+    # resources :relationships, only: [:create, :destroy]
     resources :books, only: [:create, :new, :index, :show, :destroy, :edit, :update] do
         resources :book_comments, only: [:create, :destroy]
         resource :favorites, only: [:create, :destroy]
