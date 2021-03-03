d7fdc4f48070bffbf4f046850124fbb64e37bdcc
EC2 Default User <ec2-user@ip-172-31-29-213.ec2.internal>
2021-03-03 12:31:40 +0000

    前方後方一致アクション設定

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -12,12 +12,20 @@ class SearchesController < ApplicationController
     if model == "user"
       if method == "perfect"
         User.where(name: content)
+      elsif method == "forward"
+        @user = User.where("name LIKE?", content+'%')
+      elsif method == "backward"
+        @user = User.where("name LIKE?", '%'+content)
       else
         User.where('name LIKE ?', '%'+content+'%')
       end
     elsif model == "book"
       if method == "perfect"
         Book.where(title: content)
+      elsif method == "forward"
+        @book = Book.where("title LIKE?", content+'%')
+      elsif method == "backward"
+        @user = Book.where("title LIKE?", '%'+content)
       else
         Book.where('title LIKE ?', '%'+content+'%')
       end
