a13a095248954bd5cdf03ca4688925e200a1eb1e
RyoKitatani <ryo0929m@gmail.com>
2021-03-04 12:52:36 +0000

    コメント非同期通信化完了

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -6,19 +6,22 @@ class BookCommentsController < ApplicationController
     @book_comment.user_id = current_user.id
     if @book_comment.save
       flash[:notice] = "You have commented successfully"
-      redirect_to book_path(@book)
+      # redirect_to book_path(@book)
     else
       @book_detail = Book.find(@book.id)
       @book = Book.find(@book.id)
       @user = User.find(@book_detail.user_id)
-      # @book_comment = current_user.book_comments.new(book_comment_params)
       render 'books/show'
     end 
   end
 
   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id]) 
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
+    if @book_comment.destroy
+    # redirect_to book_path(params[:book_id]) 
+    # render 'books/show'
+    end
   end
 
   private
